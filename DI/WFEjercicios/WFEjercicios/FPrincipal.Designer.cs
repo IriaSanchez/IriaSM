6977e702b5b358a3bc988195279639105ee7f6c1
Iria <iria.sanchezmedin@gmail.com>
2022-09-22 12:18:43 +0200

    WFEjercicio

--- DI/WFEjercicios/WFEjercicios/FPrincipal.Designer.cs
+++ DI/WFEjercicios/WFEjercicios/FPrincipal.Designer.cs
@@ -28,7 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(924, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 382);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(200, 382);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(724, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(200, 100);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(724, 282);
+            this.panel4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(624, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FPrincipal
             // 
@@ -36,16 +86,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(924, 482);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.Color.Coral;
             this.MaximumSize = new System.Drawing.Size(1200, 800);
             this.Name = "FPrincipal";
             this.Text = "Ejercicios Windows Form";
             this.Load += new System.EventHandler(this.FPrincipal_Load);
             this.BackColorChanged += new System.EventHandler(this.FPrincipal_BackColorChanged);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button1;
     }
 }
--- DI/WFEjercicios/WFEjercicios/FPrincipal.cs
+++ DI/WFEjercicios/WFEjercicios/FPrincipal.cs
@@ -21,11 +21,14 @@ namespace WFEjercicios
         {
             this.BackColor = Color.LimeGreen;
             this.Text = "Esta es la carga del formulario";
-            this.agregarBotonDinamico();
-        }
+            // this.agregarBotonDinamico();
+           this.LabelPanel1();
+           this.panel1.BackColor = Color.Cyan;
+           this.panel2.BackColor = Color.Chocolate;
+           this.panel3.BackColor = Color.DarkViolet;
+           this.panel4.BackColor = Color.Indigo;
+           this.button1.Text = "Salir";
 
-        private void FPrincipal_BackColorChanged(object sender, EventArgs e)
-        {
 
         }
 
@@ -37,6 +40,31 @@ namespace WFEjercicios
             btnDinamico.Location = new System.Drawing.Point(25, 25);
             btnDinamico.Size = new System.Drawing.Size(100, 25);
             this.Controls.Add(btnDinamico);
+
+        }
+
+        private void FPrincipal_BackColorChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelPanel1()
+        {
+            Label label = new Label();  
+            label.Name = "label1";
+            label.Text = "Ejercicio Windows Forms";
+            this.panel1.Controls.Add(label);
+
+            Font fuente = new Font ("Calibri" , 12);
+            label.Font = fuente;
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
 
         }
     }
